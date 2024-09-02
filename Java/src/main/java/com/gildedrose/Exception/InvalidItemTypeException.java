6dc0ce7fea3d5a2e8b79d90828fa82dcd604a9c4
Sameera-Yalla-practicals <Sameera.practicals@gmail.com>
2024-09-02 21:30:47 +0530

    Java doc for exception class

--- Java/src/main/java/com/gildedrose/Exception/InvalidItemTypeException.java
+++ Java/src/main/java/com/gildedrose/Exception/InvalidItemTypeException.java
@@ -1,10 +1,14 @@
 package com.gildedrose.Exception;
 
 /**
- *
+ * Exception thrown when there is invalid item type passed to the application.
  */
 public class InvalidItemTypeException extends RuntimeException{
 
+    /**
+     * Constructs InvalidItemTypeException with the specific detail message.
+     * @param message provides information about the exception
+     */
     public InvalidItemTypeException(String message){
         super(message);
     }
